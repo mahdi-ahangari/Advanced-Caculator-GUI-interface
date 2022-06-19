076c206e9725745ec9ecdda6d4bf2bb1d35b06dc
Mrmahdi <ahangari138010@gmail.com>
2022-06-19 03:34:55 -0700

    simple functionality to digit buttons

--- Pro Caculator.py
+++ Pro Caculator.py
@@ -1,8 +1,8 @@
 from tkinter import *
 
 
-small_font = ("arial", 16)
-big_font = ("arial", 36)
+small_font = ("arial", 20)
+big_font = ("arial", 46)
 
 
 class Caculator:
@@ -15,8 +15,8 @@ class Caculator:
         self.root.title("Caculator Mahdi Edition")
         self.root.attributes("-alpha", 0.98)
         # --------------------/ Main window / --------------------
-        self.all_nums = 0
-        self.current_num = 0
+        self.all_nums = ""
+        self.current_num = ""
         self.oFrame = self.Creat_output_frame()
         self.bFrame = self.Creat_button_frame()
         self.output = self.creat_output()
@@ -25,22 +25,23 @@ class Caculator:
             1:(3, 0),2:(3, 1),3:(3, 2),
             4:(2, 0),5:(2, 1),6:(2, 2),
             7:(1, 0),8:(1, 1),9:(1, 2),
-            0:(4, 0),"00":(4, 1),
+            0:(4, 0),"00":(4, 1),".":(4, 2)
         }
         self.num_buttons = self.creat_num_buttons()
 
         self.specialKey_list = {             # alt 0178 = ² /  alt 251 = √ / alt 0247 = ÷ / 
             "C":(0, 0),"x²":(0, 1),"√x":(0, 2),
             "x":(0, 3),"÷":(1, 3),"+":(2, 3),
-            "-":(3, 3),".":(4, 2),"=":(4, 3),
+            "-":(3, 3),"=":(4, 3),
         }
         self.special_buttons = self.creat_special_buttons()
 
         for i in range(4):
             self.bFrame.rowconfigure(i, weight=1)
             self.bFrame.columnconfigure(i, weight=1)
+        
 
-
+# --------------------/ Frames / --------------------
     def Creat_output_frame(self):
         oFrame = Frame(self.root, bg= "#2b2c33", height=200)
         oFrame.pack(expand= True,fill=BOTH)
@@ -54,25 +55,63 @@ class Caculator:
 
 
     def creat_output(self):
-        all_nums = Label(self.oFrame, text=self.all_nums, font=small_font, bg= "gray", fg="white", padx=20, pady=20, anchor=NE)
-        all_nums.pack(expand= True,fill=BOTH)
+        self.all_nums_label = Label(self.oFrame, text=self.all_nums, font=small_font, bg= "gray", fg="white", padx=8, pady=20, anchor=E)
+        self.all_nums_label.pack(expand= True,fill=BOTH)
 
-        current_num = Label(self.oFrame, text=self.current_num, font=big_font, bg= "darkgray", fg="white", padx=10, anchor=E) # #2b2c33
-        current_num.pack(expand= True,fill=BOTH)
+        self.current_num_label = Label(self.oFrame, text=self.current_num, font=big_font, bg= "gray", fg="white", padx=10, anchor=E) # #2b2c33
+        self.current_num_label.pack(expand= True,fill=BOTH)
 
-        return all_nums, current_num
+        return self.all_nums_label, self.current_num_label
+
+# --------------------/ Frames / --------------------
+
+# --------------------/ Buttons / --------------------
+    def clear_all_nums(self):
+        self.current_num = ""
+        self.all_nums = ""
+        self.all_nums_label.config(text="")
+
+
+    def clear_current_nums(self):
+        self.current_num = ""
+        self.current_num_label.config(text="")
+
+
+    def update_current_num(self, value):
+        self.current_num += str(value)
+        self.current_num_label.config(text=self.current_num)
+
+
+
+    def update_all_nums(self, value):
+        if value in ['x', '÷', '+', '-']:
+            self.all_nums += self.current_num
+            self.all_nums += str(value)
+            self.clear_current_nums()
+            self.all_nums_label.config(text=self.all_nums)
+
 
 
     def creat_num_buttons(self):
         for num, grid in self.numbers_list.items():
-            button = Button(self.bFrame, text=str(num), bg="#2b2c29", fg="white", borderwidth=0, font=("arial", 26))
+            button = Button(self.bFrame, text=str(num))
+            button.config(activeforeground="orange", activebackground="#2b2c29",
+             bg="#2b2c29", fg="white", borderwidth=0, font=("arial", 26), command=lambda x = num: self.update_current_num(x))
             button.grid(row=grid[0], column=grid[1], sticky=NSEW)
 
-    
+
     def creat_special_buttons(self):
         for key, grid in self.specialKey_list.items():
-            button = Button(self.bFrame, text=str(key), bg="#2b2c30", fg="white", borderwidth=0, font=("arial", 26))
-            button.grid(row=grid[0], column=grid[1], sticky=NSEW, )
+            button = Button(self.bFrame, text=str(key))
+            button.config(activeforeground="gold", activebackground="#2b2c30",
+             bg="#2b2c30", fg="white", borderwidth=0, font=("arial", 26), command=lambda x = key: self.update_all_nums(x))
+
+            button.grid(row=grid[0], column=grid[1], sticky=NSEW)
+            if key == "=" :
+                button.config(activeforeground="lightgray", fg="gold")
+
+
+# --------------------/ Buttons / --------------------
 
 
 
